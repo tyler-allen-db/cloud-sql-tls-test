8141503b4989d9dc6af8a2585b0e1795bb3c3673
Tyler Allen <tyler.allen@gft.com>
2021-02-01 17:32:50 +0000

    Running and connecting

--- src/main/java/com/example/cloudsqldemo/CloudSqlDemoApplication.java
+++ src/main/java/com/example/cloudsqldemo/CloudSqlDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudSqlDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudSqlDemoApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		System.exit(SpringApplication.exit(SpringApplication.run(CloudSqlDemoApplication.class, args)));
 	}
 
 }
